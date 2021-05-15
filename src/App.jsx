7943ea15171a85716a4b7b23fc2325150c2ec1a5
Daniel Castilho <dan.castilho@gmail.com>
2021-05-15 13:13:27 -0400

    Loop

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
+import ListaAlunos from './components/repeticao/ListaAlunos';
 
 import "./App.css";
 
@@ -15,6 +16,10 @@ export default () => (
 		<h1>Fundamentos React</h1>
 
 		<div className="Cards">
+			<Card titulo="#06 - Repetição" color="#FF4C65">
+				<ListaAlunos></ListaAlunos>
+			</Card>
+
 			<Card titulo="#05 - Componente com Filhos" color="#00C8F8">
 				<Familia sobrenome="Ferreira">
 					<FamiliaMembro nome="Pedro"/>
