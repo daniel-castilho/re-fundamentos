f7ef2acec34a33d64db90a357e1f650e7baec920
Daniel Castilho <dan.castilho@gmail.com>
2021-05-15 00:59:24 -0400

    Component with properties

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 import './index.css'
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro />
+        <ComParametro
+            titulo="Situação do Aluno"
+            aluno="Pedro Silva"
+            nota={9.3} />
     </div>,
     document.getElementById('root')
 )
