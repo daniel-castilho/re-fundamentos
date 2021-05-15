e9a48a28183bfd62c7ca7fd1ddadabc96b6e3fb8
Daniel Castilho <dan.castilho@gmail.com>
2021-05-15 01:44:05 -0400

    Component Aleatorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
@@ -7,6 +8,7 @@ import Fragmento from "./components/basicos/Fragmento";
 export default () => (
 	<div id="app">
 		<h1>Fundamentos React</h1>
+        <Aleatorio min={1} max={60} />
 		<Fragmento />
 		<ComParametro
 			titulo="Situação do Aluno"
