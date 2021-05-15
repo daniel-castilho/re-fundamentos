dffb26cc2f47e3cd30f841521e5fa185de08f349
Daniel Castilho <dan.castilho@gmail.com>
2021-05-15 12:58:15 -0400

    Child Component #02

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
 import React from "react";
 
-import Card from './components/leiaute/Card'
+import Card from "./components/leiaute/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
-import Familia from './components/basicos/Familia';
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 
-import './App.css';
+import "./App.css";
 
 export default () => (
 	<div className="App">
 		<h1>Fundamentos React</h1>
 
-        <div className="Cards">
-            <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
-                <Familia sobrenome="Ferreira" />
-            </Card>
+		<div className="Cards">
+			<Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+				<Familia sobrenome="Ferreira">
+					<FamiliaMembro nome="Pedro"/>
+					<FamiliaMembro nome="Ana" />
+					<FamiliaMembro nome="Gustavo" />
+				</Familia>
+			</Card>
 
-            <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
-                <Aleatorio min={1} max={60} />
-            </Card>
+			<Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+				<Aleatorio min={1} max={60} />
+			</Card>
 
-            <Card titulo="#03 - Fragmento" color="#E94C6F">
-                <Fragmento />
-            </Card>
+			<Card titulo="#03 - Fragmento" color="#E94C6F">
+				<Fragmento />
+			</Card>
 
-            <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
-                <ComParametro
-                    titulo="Situação do Aluno"
-                    aluno="Pedro Silva"
-                    nota={9.3}
-                />
-            </Card>
+			<Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+				<ComParametro
+					titulo="Situação do Aluno"
+					aluno="Pedro Silva"
+					nota={9.3}
+				/>
+			</Card>
 
-            <Card titulo="#01 - Primeiro Componente" color="#588C73">
-                <Primeiro />
-            </Card>
-        </div>
+			<Card titulo="#01 - Primeiro Componente" color="#588C73">
+				<Primeiro />
+			</Card>
+		</div>
 	</div>
 );
