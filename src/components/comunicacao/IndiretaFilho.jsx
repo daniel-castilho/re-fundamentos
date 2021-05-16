7e5197fd6e38439e238ee5afecb658883e7823e0
Daniel Castilho <dan.castilho@gmail.com>
2021-05-15 21:21:58 -0400

    Components with State

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 export default (props) => {
-
+    const min = 20;
+    const max = 50;
+    const gerarIdade = () => parseInt(Math.random() * (min)) + max;
+    const gerarNerd = () => Math.random() > 0.5
 	return (
 		<div>
 			<div>Filho</div>
 			<button
 				onClick={(e) => {
-					props.quandoClicar("João", 53, true);
+					props.quandoClicar("João", gerarIdade(), gerarNerd());
 				}}
 			>
 				Fornecer Informações
